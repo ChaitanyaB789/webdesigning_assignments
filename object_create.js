5c988edc9b8baaca685f39b520c2c792db858b7c
Chaitanya <chaitugowda789@gmail.com>
2023-07-28 12:19:27 +0530

    28.07

---
+++ object_create.js
@@ -0,0 +1,13 @@
+// Creating an object using object literal notation
+const person = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 30,
+    occupation: "Engineer",
+};
+// Accessing object properties using dot notation
+console.log(person.firstName); //Output: John
+console.log(person.lastName); //Output: Doe
+console.log(person.age); //Output: 30
+console.log(person.occupation); //Output: Engineer
+//Accessing objects properties using bracket notation
